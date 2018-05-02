9d2712fe25bd6206802e0cb5daad8b41da8db107
Alex Godwin <alexkgodwin@gmail.com>
2018-05-02 10:30:23 -0400

    Updated home directory detection for content

--- config/defaults.go
+++ config/defaults.go
@@ -1,7 +1,8 @@
 package config
 
 import (
-	"os"
+	"log"
+	"os/user"
 	"runtime"
 )
 
@@ -10,15 +11,19 @@ import (
 // NetworkDaemonDefaults - Returns the network daemon's default config.
 func NetworkDaemonDefaults() map[string]string {
 	m := make(map[string]string)
+	usr, err := user.Current()
+	if err != nil {
+		log.Fatal(err)
+	}
 
 	// TODO: Fix windows location
 	switch runtime.GOOS {
 	case "windows":
 		m["ContentDirectory"] = "/.config/gladius/gladius-networkd"
 	case "linux":
-		m["ContentDirectory"] = os.Getenv("HOME") + "/.config/gladius/gladius-content/"
+		m["ContentDirectory"] = usr.HomeDir + "/.config/gladius/gladius-content/"
 	case "darwin":
-		m["ContentDirectory"] = os.Getenv("HOME") + "/.config/gladius/gladius-content/"
+		m["ContentDirectory"] = usr.HomeDir + "/.config/gladius/gladius-content/"
 	}
 
 	return m
